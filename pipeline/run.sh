e93f67aa962381c48f6a320fd29c009132191998
Silviu Daniel Eftimie <2598911+seftimie@users.noreply.github.com>
2022-01-31 23:51:51 +0100

    First commit. Let's go

---
+++ pipeline/run.sh
@@ -0,0 +1,30 @@
+# DECLARE ALL VARS
+GCP_PROJECT_ID=""
+GCP_BUCKET=""
+BQ_DATASET_ID=""
+BQ_MODEL=""
+BQ_LOCATION=""
+BQ_FORMAT=""
+RUN_LOCATION=""
+RUN_NAME=""
+DOCKERFILE_ML=""
+SA_EMAIL=""
+SA_JSON=""
+
+# Activate service account inside container
+gcloud auth activate-service-account ${SA_EMAIL} --key-file={SA_JSON}
+
+# (todo) CREATE & TRAING BQ MODEL: create and traing your first ML model in BigQuery.
+
+# (todo) EVALUATE MODEL: evaluate how well the model performs using 
+
+# EXPORT BQ MODEL TO STORAGE
+bq --location=${BQ_LOCATION} extract --destination_format ${BQ_FORMAT}  --model ${GCP_PROJECT_ID}:${BQ_DATASET_ID}.${BQ_MODEL} ${GCP_BUCKET}
+
+# DEPLOY MODEL INTO CLOUD RUN
+mkdir /models/tf_models/model/00000001/;
+gsutil cp -r ${GCP_BUCKET}/* /models/tf_models/model/00000001/ ;
+wget -O Dockerfile ${DOCKERFILE_ML} ;
+gcloud builds submit --project=${GCP_PROJECT_ID} --tag gcr.io/${GCP_PROJECT_ID}/${RUN_NAME} ;
+gcloud run deploy ${RUN_NAME} --project=${GCP_PROJECT_ID} --image gcr.io/${GCP_PROJECT_ID}/${RUN_NAME} --platform managed --region ${RUN_LOCATION} --allow-unauthenticated;
+gcloud run services add-iam-policy-binding ${RUN_NAME} --project=${GCP_PROJECT_ID} --region ${RUN_LOCATION} --member="allUsers" --role="roles/run.invoker"
